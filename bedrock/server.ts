d89f11d6610208825af47d3d6dc552802158beb7
Samantha-uk <samantha.uk.github@gmail.com>
2021-02-06 10:31:59 +0000

    logo update

--- bedrock/server.ts
+++ bedrock/server.ts
@@ -1,5 +1,20 @@
+import { Server } from "https://deno.land/x/wocket@v0.6.2/mod.ts";
 import { Drash } from "./deps.ts";
 import ZigzagPanelResource from "./resources/resource-zigzag-panel.ts";
+
+// Create the server
+const wsserver = new Server();
+
+// Run the server
+wsserver.run({
+  hostname: "0.0.0.0",
+  port: 1962,
+});
+
+console.log(
+  `Bedrock WS started on ${wsserver.hostname}:${wsserver.port}`,
+);
+
 
 export const server = new Drash.Http.Server({
   static_paths: ["/zigzag"],
